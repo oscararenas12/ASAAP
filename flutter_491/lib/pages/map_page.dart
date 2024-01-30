28b4fb60fb35f5df96d0df01b1db4e0e76b6917c
Erick Garcia <73147193+ErickGarcia23@users.noreply.github.com>
2024-01-29 20:08:24 -0800

    up

--- flutter_491/lib/pages/map_page.dart
+++ flutter_491/lib/pages/map_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_491/components/toolbar.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:latlong2/latlong.dart';
+
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -12,7 +14,7 @@ class MapPage extends StatelessWidget {
       appBar: Toolbar(title: 'map'),
       body: FlutterMap(
         options: MapOptions(
-          center: LatLng(33.7838, -118.1141),zoom: 10,), 
+          center: LatLng(33.7838, -118.1141),zoom: 16,), 
           
         children: [
 
@@ -21,16 +23,20 @@ class MapPage extends StatelessWidget {
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName: 'dev.fleaflet.flutter_map.example',
           ),
-
+//
 //          MarkerLayer(
 //            markers: [
 //
 //              Marker(
-//                point: LatLng(51.1212, 51.535),
-//                builder:(context){
+//                width: 100,
+//                point: LatLng(33.7838, -118.1141),
+//                builder: (context){
 //                  return Column(
 //                    children: [
 //                      Container(
+//                        padding: const EdgeInsets.symmetric(
+//                          horizontal: 8, vertical: 4
+//                        ),
 //                        decoration: BoxDecoration(
 //                          color: Colors.white,
 //                          borderRadius: BorderRadius.all(Radius.circular(14))
@@ -41,14 +47,17 @@ class MapPage extends StatelessWidget {
 //
 //                        ),
 //                      )
+//                      
 //                    ],
 //
 //                  );
-//                }
+//                }, child: SvgPicture.asset('User_circle.svg'),
 //                
 //                )
- //           ],
-   //       ),
+//            ],
+//          ),
+//
+// ^^^^^
         ],
         
       ),
