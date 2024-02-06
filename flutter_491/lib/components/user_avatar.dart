b73cb4ab97133880485e4f271ac2bda9d908be4a
Erick Garcia <73147193+ErickGarcia23@users.noreply.github.com>
2024-02-05 20:31:47 -0800

    Update Avatar

--- flutter_491/lib/components/user_avatar.dart
+++ flutter_491/lib/components/user_avatar.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_491/components/toolbar.dart';
+import 'package:fluttermoji/fluttermojiCircleAvatar.dart';
+import 'package:fluttermoji/fluttermojiCustomizer.dart';
+import 'dart:math';
+import 'package:fluttermoji/fluttermojiSaveWidget.dart';
+import 'package:fluttermoji/fluttermojiThemeData.dart';
+
 
 class UserAvatar extends StatelessWidget {
 
   final double size;
   const UserAvatar({super.key,  this.size = 50});
 
-  @override
+@override
   Widget build(BuildContext context) {
-
-    //profile avatar
-    return ClipRRect(  //image bordering
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-      child: Image.asset('assets/temp/User_cicrle.png',
-      width: size,
-      height: size,
+    var _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: Toolbar(title: "Customize AI"),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: FluttermojiCircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey[200],
+                ),
+              ),
+              SizedBox(
+                width: min(600, _width * 0.85),
+                child: Row(
+                  children: [
+                    Text(
+                      "Customize:",
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Spacer(),
+                    FluttermojiSaveWidget(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 30),
+                child: FluttermojiCustomizer(
+                  scaffoldWidth: min(600, _width * 0.85),
+                  autosave: false,
+                  theme: FluttermojiThemeData(
+                      boxDecoration: BoxDecoration(boxShadow: [BoxShadow()])),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
