c25fca2176f5ca23856ed95c6ce8525e192e8cd8
el_o <72949326+oscararenas12@users.noreply.github.com>
2024-02-14 22:54:36 -0800

    Calendar Update
    Started using https://pub.dev/packages/calendar_view to complete calendar. This is just the foundation of it. You can click on day and it will be blue. You can click on the year and it will have another pop up. Still underconstrution.

---
+++ flutter_491/lib/pages/calendar_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:calendar_view/calendar_view.dart';
+
+class FullScreenCalendarPage extends StatefulWidget {
+  @override
+  _FullScreenCalendarPageState createState() => _FullScreenCalendarPageState();
+}
+
+class _FullScreenCalendarPageState extends State<FullScreenCalendarPage> {
+  late EventController _eventController;
+  DateTime? _selectedDay; // Variable to track the selected day
+
+  @override
+  void initState() {
+    super.initState();
+    _eventController = EventController();
+  }
+
+  // This method is used to build the MonthView.
+  Widget _buildMonthView() {
+    return MonthView(
+      controller: _eventController,
+      cellBuilder: (date, events, isToday, isInMonth) {
+        final bool isSelectedDay =
+            _selectedDay != null && date.isAtSameMomentAs(_selectedDay!);
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              print('Tapped on ${date.day}');
+              _selectedDay = date; // Update the selected day
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: isSelectedDay
+                  ? Colors.blue[100]
+                  : (isToday ? Colors.blue : Colors.transparent),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    top: 8.0), // Add padding at the top for better positioning
+                child: Text(
+                  '${date.day}',
+                  style:
+                      TextStyle(color: isInMonth ? Colors.black : Colors.grey),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      minMonth: DateTime.now().subtract(Duration(days: 365)), // one year back
+      maxMonth: DateTime.now().add(Duration(days: 365)), // one year forward
+      initialMonth: DateTime.now(),
+      startDay: WeekDays.sunday,
+      onEventTap: (event, date) => print('Tapped on event $event'),
+      onDateLongPress: (date) => print('Long pressed date $date'),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CalendarControllerProvider(
+      controller: _eventController,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Calendar'),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.white, // Background color of the container
+          ),
+          child: _buildMonthView(), // The MonthView builder method
+        ),
+      ),
+    );
+  }
+}
